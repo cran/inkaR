5e604ee49454ee5ee1701579c45f6a914fd5ca08
Omer Furkan Coban <oemer.furkan.coban@uni-oldenburg.de>
2026-06-29 13:00:24 +0000

    version 0.6.6

--- inst/doc/introduction.R
+++ inst/doc/introduction.R
@@ -1,6 +1,7 @@
 ## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
     collapse = TRUE,
-    comment = "#>"
+    comment = "#>",
+    eval = FALSE
 )
 
